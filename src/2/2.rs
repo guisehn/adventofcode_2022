24d972a477185078a7aad9236f4a186775e3d8bf
Guilherme Sehn <guilherme@hey.com>
2022-12-02 12:36:58 -0300

    refactor solution 2

--- src/2/2.rs
+++ src/2/2.rs
@@ -14,11 +14,19 @@ enum Outcome {
     Lose,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
-struct Round(Shape, Shape, Outcome);
+struct Round {
+    me: Shape,
+    opponent: Shape,
+    outcome: Outcome,
+}
 
 #[derive(Debug, Clone, Copy)]
-struct RoundGoal(Shape, Outcome);
+struct PartialRound {
+    opponent: Shape,
+    outcome: Outcome,
+}
 
 fn main() {
     let input = read_input();
@@ -35,18 +43,18 @@ fn read_input() -> String {
 fn get_rounds(input: String) -> Vec<Round> {
     input
         .split("\n")
-        .map(|line| line.split(" "))
         .map(|line| {
             let chars: Vec<char> = line
+                .split(" ")
                 .flat_map(|shape| shape.to_string().chars().collect::<Vec<char>>())
                 .collect();
 
-            let round_goal = RoundGoal(
-                char_to_shape(*chars.get(0).expect("Missing 1st char")),
-                char_to_outcome(*chars.get(1).expect("Missing 2nd char")),
-            );
+            let round_goal = PartialRound {
+                opponent: char_to_shape(*chars.get(0).expect("Missing 1st char")),
+                outcome: char_to_outcome(*chars.get(1).expect("Missing 2nd char")),
+            };
 
-            to_round(round_goal)
+            complete_round(round_goal)
         })
         .collect()
 }
@@ -69,10 +77,34 @@ fn char_to_outcome(c: char) -> Outcome {
     }
 }
 
-fn get_score(round: &Round) -> u32 {
-    let Round(_opponent_shape, my_shape, outcome) = round;
+fn complete_round(partial: PartialRound) -> Round {
+    let PartialRound { opponent, outcome } = partial;
 
-    let shape_score = match my_shape {
+    let me = match (opponent, outcome) {
+        (Shape::Rock, Outcome::Win) => Shape::Paper,
+        (Shape::Rock, Outcome::Lose) => Shape::Scissors,
+        (Shape::Rock, Outcome::Draw) => Shape::Rock,
+
+        (Shape::Paper, Outcome::Win) => Shape::Scissors,
+        (Shape::Paper, Outcome::Lose) => Shape::Rock,
+        (Shape::Paper, Outcome::Draw) => Shape::Paper,
+
+        (Shape::Scissors, Outcome::Win) => Shape::Rock,
+        (Shape::Scissors, Outcome::Lose) => Shape::Paper,
+        (Shape::Scissors, Outcome::Draw) => Shape::Scissors,
+    };
+
+    Round {
+        opponent,
+        me,
+        outcome,
+    }
+}
+
+fn get_score(round: &Round) -> u32 {
+    let Round { me, outcome, .. } = round;
+
+    let shape_score = match me {
         Shape::Rock => 1,
         Shape::Paper => 2,
         Shape::Scissors => 3,
@@ -85,24 +117,4 @@ fn get_score(round: &Round) -> u32 {
     };
 
     shape_score + outcome_score
-}
-
-fn to_round(round_goal: RoundGoal) -> Round {
-    let RoundGoal(opponent_shape, outcome) = round_goal;
-
-    let my_shape = match round_goal {
-        RoundGoal(Shape::Rock, Outcome::Win) => Shape::Paper,
-        RoundGoal(Shape::Rock, Outcome::Lose) => Shape::Scissors,
-        RoundGoal(Shape::Rock, Outcome::Draw) => Shape::Rock,
-
-        RoundGoal(Shape::Paper, Outcome::Win) => Shape::Scissors,
-        RoundGoal(Shape::Paper, Outcome::Lose) => Shape::Rock,
-        RoundGoal(Shape::Paper, Outcome::Draw) => Shape::Paper,
-
-        RoundGoal(Shape::Scissors, Outcome::Win) => Shape::Rock,
-        RoundGoal(Shape::Scissors, Outcome::Lose) => Shape::Paper,
-        RoundGoal(Shape::Scissors, Outcome::Draw) => Shape::Scissors,
-    };
-
-    Round(opponent_shape, my_shape, outcome)
 }
